b9d44279d47c2fb1fa788dd00046cc1f494cf68a
Eugene Allenov <e.allenov@gmail.com>
2017-01-04 14:42:35 +0300

    Added ES6 support for webpack && Webpack base config mergin

---
+++ webpack.config.babel.js
@@ -0,0 +1,65 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { optimize, HotModuleReplacementPlugin } from 'webpack'
+import Config from 'webpack-config'
+import Path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import Autoprefixer from 'autoprefixer'
+
+export default new Config().merge({
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loaders: ['eslint'],
+        include: [
+          Path.resolve(__dirname, 'src'),
+        ],
+      },
+    ],
+    loaders: [
+      {
+        test: /\.js?$/,
+        include: Path.join(__dirname, 'src'),
+        loaders: ['babel'],
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png|svg)$/,
+        include: Path.join(__dirname, 'src'),
+        loader: 'url-loader?limit=5120&name=/img/[name].[ext]',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, 'index.html'),
+      inject: 'body',
+      hash: true,
+    }),
+
+    new optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'js/libraries.js',
+      minChunks: () => module.resource && module.resource.indexOf(Path.resolve(__dirname, 'src')) === -1,
+    }),
+
+    new HotModuleReplacementPlugin(),
+  ],
+
+  postcss: () => [Autoprefixer],
+
+  resolve: {
+    root: [
+      Path.resolve('node_modules'),
+      Path.resolve('src'),
+    ],
+    alias: {
+      less: Path.resolve(__dirname, 'src/less/'),
+      components: Path.resolve(__dirname, 'src/components/'),
+      actions: Path.resolve(__dirname, 'src/actions/'),
+      constants: Path.resolve(__dirname, 'src/constants/'),
+      reducers: Path.resolve(__dirname, 'src/reducers/'),
+      routes: Path.resolve(__dirname, 'src/routes/'),
+    },
+  },
+});
---
+++ webpack.prod.config.babel.js
@@ -0,0 +1,45 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { DefinePlugin, optimize } from 'webpack'
+import Config from 'webpack-config'
+import Path from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
+
+export default new Config().extend('webpack.config.babel.js').merge({
+  entry: {
+    app: './src',
+  },
+
+  output: {
+    path: './build/',
+    filename: '/js/app.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.less$/,
+        include: Path.join(__dirname, 'src'),
+        loader: ExtractTextPlugin.extract('css!postcss!less'),
+      },
+    ],
+  },
+
+  plugins: [
+    new DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+
+    new optimize.UglifyJsPlugin({ sourceMap: false }),
+
+    new ExtractTextPlugin('/css/app.css'),
+
+    new CleanWebpackPlugin(['build'], {
+      root: Path.resolve(__dirname),
+      verbose: true,
+      dry: false,
+    }),
+  ],
+});
