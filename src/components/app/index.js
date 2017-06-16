665bd7d8f2dceccc06f8d1ebb4bec8bea30a4cdd
Eugene Allenov <e.allenov@gmail.com>
2017-06-16 12:11:57 +0800

    rename less component to unique style.less

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Header } from 'components'
-import styles from './app.less'
+import styles from './styles.less'
 
 const App = (props) => {
   const { children } = props
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './header.less'
+import styles from './styles.less'
 
 const Header = () =>
   <div className={styles.root} />
